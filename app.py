c6f0a2f3ea65ef1a6191ff923fd7f4210bb71cd3
michaelhesch <michael.hesch@gmail.com>
2021-09-02 13:09:14 +0100

    Add basic Flask setup

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-# main app placeholder
+import os
+import pymongo
+from flask import Flask, render_template
+
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello World"
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+    port=int(os.environ.get("PORT")),
+    debug=True) # UPDATE TO DEBUG = FALSE BEFORE DEPLOYMENT
