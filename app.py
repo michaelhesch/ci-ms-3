b483c9786a3ed08718679f0540dbf9ce2e9c4525
michaelhesch <michael.hesch@gmail.com>
2021-09-02 13:31:39 +0100

    Configure DB connection and test query

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
 import os
-import pymongo
-from flask import Flask, render_template
-
+from flask_pymongo import PyMongo
+from flask import (
+    Flask, render_template, flash, 
+    redirect, request, session, url_for)
+from bson.objectid import ObjectId
+# Import environment variables for local development
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+# Import environment variables for MongoDB
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 @app.route("/")
-def hello_world():
-    return "Hello World"
+@app.route("/home")
+def home():
+    users = mongo.db.users.find()
+    return render_template("index.html", users=users)
 
 
 if __name__ == "__main__":
