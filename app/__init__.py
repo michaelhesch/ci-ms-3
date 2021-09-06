5f10b014efb2e0e71cbcecd45cbe1b765c78d6ff
michaelhesch <michael.hesch@gmail.com>
2021-09-06 10:31:16 +0100

    Update endpoint for main content to fix routing problems

--- app/__init__.py
+++ app/__init__.py
@@ -39,4 +39,7 @@ def create_app(config_class=Config):
     from app.auth import bp as auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
 
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp, url_prefix='/main')
+
     return app
