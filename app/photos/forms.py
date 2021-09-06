5d3508d28a322cfa5375cb678762be91055d4e58
michaelhesch <michael.hesch@gmail.com>
2021-09-06 19:51:39 +0100

    Add photo feed blueprint and routing

---
+++ app/photos/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
+from app.models import User
+
+# Photo upload form setup
+# class PhotoForm(FlaskForm):
+    
