6be48a5339a25fcb56b0c61562afe3b4d080cfe1
michaelhesch <michael.hesch@gmail.com>
2021-09-06 11:57:30 +0100

    Update app config

--- config.py
+++ config.py
@@ -1,17 +1,21 @@
 import os
 import cloudinary
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
-    # MONGO_DBNAME = os.environ.get("MONGO_DBNAME")
-    # MONGO_URI = os.environ.get("MONGO_URI")
-    # Import flask secret key
+    # Import flask secret key environment variable
     SECRET_KEY = os.environ.get("SECRET_KEY")
 
+    # Import cloudinary configuration environment variables
     cloudinary.config(
         cloud_name = os.getenv('CLOUD_NAME'), 
         api_key = os.getenv('API_KEY'), 
         api_secret = os.getenv('API_SECRET'))
 
+    # Import MongoDB configuration environment variables
     MONGODB_SETTINGS = {
         'db': os.environ.get("MONGO_DB"),
         'host': os.environ.get("MONGO_URI")
