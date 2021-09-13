2b98f491f4b829a73b5ca76d9309536ec9f77afb
michaelhesch <michael.hesch@gmail.com>
2021-09-13 15:41:13 +0100

    Add 403 error handling and improve error template styling

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -1,6 +1,10 @@
 from flask import render_template
 from app.errors import bp
 
+@bp.app_errorhandler(403)
+def not_found_error(error):
+    return render_template('errors/403.html', title='403 Error'), 404
+
 @bp.app_errorhandler(404)
 def not_found_error(error):
     return render_template('errors/404.html', title='404 Error'), 404
